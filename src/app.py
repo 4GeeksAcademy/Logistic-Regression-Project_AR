8fc696fd764e4526daad57a98a5df317a28cca2b
ARA-2024 <anarubioa@yahoo.es>
2025-01-17 20:36:05 +0000

    First Commit

--- src/app.py
+++ src/app.py
@@ -1,4 +1,31 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import missingno as msno
+from sklearn.impute import KNNImputer
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error as MSE
+from sklearn.preprocessing import LabelEncoder
+import xgboost as xgb
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.datasets import make_regression
+from xgboost import XGBRegressor
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from lazypredict.Supervised import LazyRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 
-# your code here
+# Load the dataset
+df = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/logistic-regression-project-tutorial/main/bank-marketing-campaign-data.csv")
+
+# Print the first rows of the dataframe for visualization
+df.head()
+
+df.shape
+df.head().T
+df.info()
+
+# Start Exploratory data analysis (EDA)
